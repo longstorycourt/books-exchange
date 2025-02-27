313de041014da1560f973652c66acb4d631b1cbf
lin <intofolkloree@gmail.com>
2025-02-27 20:11:45 +0700

    bookSwapPage
    bookSwapPage acceptable

--- js files/bookSwapPage.js
+++ js files/bookSwapPage.js
@@ -1,5 +1,6 @@
 
 const headerContent = document.getElementById("header");
+const footerContent = document.getElementById("footer");
 
 let navItems = [
     {
@@ -7,16 +8,16 @@ let navItems = [
         navItem: `Home`,
         href:`#`
     },
-    {
-        id: `2`,
-        navItem: `About`,
-        href:`#footer`
-    },
-    {
-        id: `3`,
-        navItem: `Books4Swap`,
-        href:`#books4Swap`
-    },
+    // {
+    //     id: `2`,
+    //     navItem: `About`,
+    //     href:`#about-us`
+    // },
+    // {
+    //     id: `3`,
+    //     navItem: `Books4Swap`,
+    //     href:`#books4Swap`
+    // },
     {
         id: `4`,
         navItem: `Contact`,
@@ -25,13 +26,13 @@ let navItems = [
 ];
 let navItemsHtml = ``;
 for(let i of navItems){
-    navItemsHtml += `<a class="nav-items text-uppercase fw-bold text-dark mx-0 mx-lg-2 text-decoration-none" href="${i.href}">${i.navItem}</a>`;
+    navItemsHtml += `<a class="nav-items text-uppercase text-dark mx-0 mx-lg-2 text-decoration-none" href="${i.href}">${i.navItem}</a>`;
 }
 
 headerContent.innerHTML = `
     <nav class="navbar fixed-top navbar-light navbar-expand-lg" style="background-color: #e3f2fd;">
         <div class="container px-1 px-md-2 px-lg-3 mx-auto">
-            <a class="navbar-brand link-dark fw-bold" href="#">Books4All</a>
+            <a class="navbar-brand link-dark" style="font-size: larger;" href="#"><i class="fa-solid fa-book-open me-2"></i>Books4All</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
             </button>
@@ -44,7 +45,6 @@ headerContent.innerHTML = `
     </nav>`;
 
 let swapLists = document.getElementById("swapLists");
-
 let swapItems = [
     {
         id: `001`,
@@ -126,3 +126,39 @@ for(let i = 0; i < 9; i++){
 
 }
 
+
+footerContent.innerHTML += `
+    <div class="row">
+        <div class="col-12 col-md-6 col-lg-4">
+            <h3>Books4All</h3>
+            <hr class="w-25">
+            <ul class="list-unstyled">
+                <li>About Us</li>
+                <li>Our Services</li>
+                <li>Privacy Policy</li>
+            </ul>
+        </div>
+        <div class="col-12 col-md-6 col-lg-4">
+            <h3>Find Us On</h3>
+            <hr class="w-25">
+            <ul class="list-unstyled">
+                <li><a href="fb.com" class="text-decoration-none link-dark" title="Visit our Facebook Page" target="_blank" >Facebook</a></li>
+                <li><a href="instagram.com" class="text-decoration-none link-dark" title="Visit our Instagram Page" target="_blank">Instagram</a></li>
+                <li><a href="#" class="text-decoration-none link-dark" title="Visit our Twitter Page" target="_blank">Twitter</a></li>
+            </ul>
+        </div>
+        <div class="col-12 col-md-6 col-lg-4">
+            <form action="">
+                <h3 class="">Subscribe to our newsletter</h3>
+                <p class="d-none d-lg-block">To follow the lastest updates</p>
+                <hr class="w-25">
+
+                <div class="d-flex flex-column">
+                <input class="mb-2 rounded-1 border-light" id="email" type="email" placeholder="you@example.com"/>
+                <button class="btn btn-dark me-auto ms-lg-auto py-1" style="width: fit-content;">Submit</button>
+                </div>
+            </form>
+        </div>
+    </div>
+    </div>
+`;
