a70bd4f72ae4e5342b7b94065d812ae8a2561c6d
lin <intofolkloree@gmail.com>
2025-02-27 12:58:43 +0700

    Summary

--- index.js
+++ index.js
@@ -3,37 +3,37 @@ let swapLists = document.getElementById("swapLists");
 let saleLists = document.getElementById("saleLists");
 
 let services =[
-{
-    id: '1',
-    img: 'https://i.pinimg.com/736x/ab/67/fb/ab67fbeb22c88e58d85166785e5c7dac.jpg',
-    alt: 'stacks of books',
-    cardTitle1: 'Need to free up',
-    cardTitle2: 'Books?',
-    cardText: 'If you wish to put your books up for sale, click the button below.',
-    btnText: 'Sell your book',
-    link:'/signUpPage.html'
-},
-{
-    id: '2',
-    img: 'https://i.pinimg.com/736x/17/2d/2c/172d2c44ea316d1a70dbb4fd53bf8696.jpg',
-    alt: 'stacks of books',
-    cardTitle1: 'Wanna swap your books',
-    cardTitle2: 'with other?',
-    cardText: 'Register your books with us to find the ideal candidate to exchange them with.',
-    btnText: 'Exchange your book',
-    link:'/signUpPage.html'
-}
+    {
+        id: '1',
+        img: 'https://i.pinimg.com/736x/17/2d/2c/172d2c44ea316d1a70dbb4fd53bf8696.jpg',
+        alt: 'stacks of books',
+        cardTitle1: 'Register Your Books',
+        cardTitle2: '',
+        cardText: 'Register your books to find the ideal candidate to exchange them with.',
+        btnText: 'Register Your Book Now',
+        link:'/bookListingForm.html'
+    },
+    {
+        id: '2',
+        img: 'https://i.pinimg.com/736x/ab/67/fb/ab67fbeb22c88e58d85166785e5c7dac.jpg',
+        alt: 'stacks of books',
+        cardTitle1: 'Browse Book Offers',
+        cardTitle2: '',
+        cardText: 'See the listed book offers up for grab.',
+        btnText: 'Check Out Book Offers',
+        link:'/bookSwapPage.html'
+    }
 ];
 
 for (let i of services) {
 serviceContent.innerHTML += `
-    <div class="col-12 col-md-6">
+    <div class="col-12 col-lg-6">
     <div class="card border-0 rounded-0 bg-dark text-white h-100">
         <img class="card-img-body px-0 img-fluid h-100 object-fit-cover" src="${i.img}" alt="${i.alt}" style="max-height:500px;">
         <div class="card-img-overlay d-flex flex-column justify-content-end px-3 px-lg-5">
-            <h5 class="card-title"><span class="bg-dark bg-opacity-25">${i.cardTitle1}</span><br> <span class="bg-dark bg-opacity-25">${i.cardTitle2}</span></h5>
-            <p class="card-text d-none d-md-block"><span class="bg-dark bg-opacity-25">${i.cardText}</span></p>
-            <a class="btn btn-dark" href="${i.link}" role="button" style="width: fit-content;">${i.btnText}</a>
+            <h5 class="card-title"><span class="bg-secondary bg-opacity-10">${i.cardTitle1}</span><br> <span class="bg-dark bg-opacity-25">${i.cardTitle2}</span></h5>
+            <p class="card-text d-none d-md-block"><span class="bg-secondary bg-opacity-10">${i.cardText}</span></p>
+            <a class="btn btn-primary btn-lg" href="${i.link}" role="button" style="width: fit-content;">${i.btnText}</a>
         </div>
         </div>
     </div>
@@ -55,6 +55,18 @@ let swapItems = [
     },
     {
         id: `003`,
+        title: `Everything I know about love`,
+        img: `https://i.pinimg.com/736x/74/ba/8c/74ba8c684044bc9097393554394f3cf7.jpg`,
+        author: `Dolly Alderton`,
+    },
+    {
+        id: `004`,
+        title: `Everything I know about love`,
+        img: `https://i.pinimg.com/736x/74/ba/8c/74ba8c684044bc9097393554394f3cf7.jpg`,
+        author: `Dolly Alderton`,
+    },
+    {
+        id: `005`,
         title: `Everything I know about love`,
         img: `https://i.pinimg.com/736x/74/ba/8c/74ba8c684044bc9097393554394f3cf7.jpg`,
         author: `Dolly Alderton`,
@@ -95,6 +107,13 @@ let saleItems = [
     },
     {
         id: `003`,
+        title: `Everything I know about love`,
+        img: `https://i.pinimg.com/736x/74/ba/8c/74ba8c684044bc9097393554394f3cf7.jpg`,
+        author: `Dolly Alderton`,
+        price: `$5.99`
+    },
+    {
+        id: `004`,
         title: `Everything I know about love`,
         img: `https://i.pinimg.com/736x/74/ba/8c/74ba8c684044bc9097393554394f3cf7.jpg`,
         author: `Dolly Alderton`,
